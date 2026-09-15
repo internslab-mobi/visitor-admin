416bab15e32441e6db75d149f9987752d66feb28
internslab-mobi <interns@gomobi.io>
2026-09-15 15:16:02 +0530

    Add health endpoint and update project package

--- vms/src/main/java/xyz/mobi/visitormanagementsystem/VMSApplication.java
+++ vms/src/main/java/xyz/mobi/visitormanagementsystem/VMSApplication.java
@@ -1,4 +1,4 @@
-package visitormanagementsystem;
+package xyz.mobi.visitormanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- vms/src/test/java/xyz/mobi/visitormanagementsystem/VmsApplicationTests.java
+++ vms/src/test/java/xyz/mobi/visitormanagementsystem/VmsApplicationTests.java
@@ -1,4 +1,4 @@
-package visitormanagementsystem;
+package xyz.mobi.visitormanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
