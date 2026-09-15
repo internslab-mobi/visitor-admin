2c624a50a34aa856ce88dabde7ac54bfd729967a
internslab-mobi <interns@gomobi.io>
2026-09-15 16:28:05 +0530

    rename package Register to register

--- vms/src/main/java/xyz/mobi/visitormanagementsystem/registration/preRegistration.java
+++ vms/src/main/java/xyz/mobi/visitormanagementsystem/registration/preRegistration.java
@@ -1,4 +1,4 @@
-package xyz.mobi.visitormanagementsystem.Registration;
+package xyz.mobi.visitormanagementsystem.registration;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
