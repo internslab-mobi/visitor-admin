98c565d25fcf867f84ac1ba5ef3edcae8c74c9d6
Anisha Z <anisha.z@mobiasia.com>
2026-09-15 14:11:45 +0530

    springboot template with dependency added and mysql connected

---
+++ vms/src/main/java/visitormanagementsystem/VMSApplication.java
@@ -0,0 +1,13 @@
+package visitormanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VMSApplication.class, args);
+	}
+
+}
