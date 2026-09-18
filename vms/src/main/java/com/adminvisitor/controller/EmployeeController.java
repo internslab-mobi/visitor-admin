678f6a3fbb136e9ec6284901abafc398c7d8f104
Nethra R <nethra.r@mobiasia.com>
2026-09-18 15:16:50 +0530

    Employee api addition

---
+++ vms/src/main/java/com/adminvisitor/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.adminvisitor.controller;
+
+import com.adminvisitor.entity.Employee;
+import com.adminvisitor.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
